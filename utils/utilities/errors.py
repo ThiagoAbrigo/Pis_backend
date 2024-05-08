ce56f2250e222e063418bd7ad7197d3bf0c404e0
Santiago <santiabrigo497@gmail.com>
2024-05-07 22:56:07 -0500

    updated save_person method

--- utils/utilities/errors.py
+++ utils/utilities/errors.py
@@ -1,9 +1,11 @@
 class Errors:
     error = {
         "-1": "Role not found",
-        "-2": "password repeat",
+        "-2": "email repeat",
         "-3": "person not found",
-        "-6": "account does not exist",
+        "-4": "error in the transaction",
         "-5": "token not found",
+        "-6": "account does not exist",
         "-7": "token invalid",
+        "-8": "invalid ID",
     }
