c2e89d63b5277e2b1da40b17e5b2f4506a5eb464
Santiago <santiabrigo497@gmail.com>
2024-06-24 11:11:21 -0500

    changes in the model-person

--- controllers/personController.py
+++ controllers/personController.py
@@ -21,8 +21,6 @@ class PersonController:
                 "external_id": person.external_id,
                 "name": person.name,
                 "lastname": person.lastname,
-                "phone": person.phone,
-                "identification": person.identification,
                 "email": account.email if account else None,
                 "status": account.status if account else None,
                 "rol": rol.rol if rol else None,
@@ -30,24 +28,6 @@ class PersonController:
             person_List.append(person_data)
         return person_List
 
-    def validate_ID(self, identification):
-        identification_str = str(identification)
-
-        if len(identification_str) != 10:
-            return False
-
-        coeficientes = [2, 1, 2, 1, 2, 1, 2, 1, 2]
-        suma = 0
-        for i in range(9):
-            digito = int(identification_str[i]) * coeficientes[i]
-            suma += digito if digito < 10 else digito - 9
-
-        total = suma % 10 if suma % 10 == 0 else 10 - suma % 10
-
-        if total == int(identification_str[9]):
-            return True
-        else:
-            return False
 
     def validate_Email(self, email):
         format = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
@@ -57,22 +37,10 @@ class PersonController:
         else:
             return False
 
-    def validate_Phone(self, phone):
-        format = r"^[0-9]{10}$"
-
-        if re.match(format, phone):
-            return True
-        else:
-            return False
-
     def save_person(self, data):
         repeated_account = Account.query.filter_by(email=data["email"]).first()
         if repeated_account:
             return -2
-        
-        repeated_identification = Person.query.filter_by(identification=data["identification"]).first()
-        if repeated_identification:
-            return -9
         
         person = Person()
 
@@ -84,15 +52,11 @@ class PersonController:
         else:
             rol = Rol.query.filter_by(rol="Usuario").first()
 
-        if not self.validate_ID(data["identification"]):
-            return -8
-        elif not self.validate_Email(data["email"]):
+        if not self.validate_Email(data["email"]):
             return -11
 
         person.name = data["name"]
         person.lastname = data["lastname"]
-        person.phone = data["phone"]
-        person.identification = data["identification"]
         person.external_id = uuid.uuid4()
         person.rol_id = rol.id
         db.session.add(person)
@@ -126,17 +90,13 @@ class PersonController:
     def modify_person(self, external_id, data):
         person = Person.query.filter_by(external_id=external_id).first()
         if person:
-            if not self.validate_ID(data["identification"]):
-                return -8
-            elif not self.validate_Email(data["email"]):
+            if not self.validate_Email(data["email"]):
                 return -11
             rol_name = data["rol"]
             rol = Rol.query.filter_by(rol=rol_name).first()
             if rol:
                 person.name = data.get("name", person.name)
                 person.lastname = data.get("lastname", person.lastname)
-                person.phone = data.get("phone", person.phone)
-                person.identification = data.get("identification", person.identification)
                 person.rol_id = rol.id
                 if 'email' in data or 'password' in data:
                     account = Account.query.filter_by(person_id=person.id).first()
